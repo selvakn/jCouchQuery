afae1a6bf472edaec0d8bebb3d8a7cd6fd0a7003
Selva <k.n.selvakumar@gmail.com>
2010-04-04 23:28:54 +0530

    authentication - completed. Manipulating documents - wip

--- spec/unit/spec.js
+++ spec/unit/spec.js
@@ -1,19 +1,38 @@
 describe 'jCouchQuery'
   before_each
-    $.jcouch.init({})
+    stub(jQuery, 'ajax')
+    $.jcouch.init(testDatabaseOptions);
+    
     responses = {
-      database_exists : json_fixture("database_exists")
-    }
+      database_exists : json_fixture("databaseExists")
+    };
+  end
+  
+  describe 'login'
+    it 'should post to _session with username and password'
+      
+      $.jcouch.init(testDatabaseOptions);
+    end
   end
   
   describe 'couchDatabase'
     it 'should give the database string from the current windown location'
-      $.jcouch.couchDatabase.connectionString().should.be "http://127.0.0.1"
+      $.jcouch.couchDatabase.connectionString().should.be "/couch_database"
     end
-    
-    it 'should do something'
-      
-    end
-    
   end
+    
+  describe 'constructUrl'
+    before_each 
+      databaseStr = $.jcouch.couchDatabase.connectionString();
+    end
+    
+    it 'should construct the url from the resource information passed'
+      $.jcouch._constructUrl("resource1").should.be databaseStr + "/resource1"
+    end
+    
+    it 'should construct from multiple arguments'
+      $.jcouch._constructUrl("resource1", "subResource").should.be databaseStr + "/resource1/subResource"
+    end
+  end
+  
 end
